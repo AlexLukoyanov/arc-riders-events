2f51223c62952d5dfb41d986d51a4715e5622f2f
Alex Lukoyanov <lukoav@gmail.com>
2025-12-11 18:29:04 +0300

    fix: fix delpoy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/arc-event/',
+  base: '/arc-riders-events/',
   server: {
     proxy: {
       '/api': {
